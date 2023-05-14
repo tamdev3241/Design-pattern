bf45c4f85ccf3b8f139e65d1f630aabe90d6fb46
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-05-14 10:22:03 +0700

    singleton pattern

---
+++ singleton_pattern/lib/app_logger.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:logging/logging.dart';
+
+/// Abstract logger class to showcase creation of a Singleton
+/// wrapper as an extension
+///
+abstract class BaseLogger {
+  @protected
+  late Logger _logger;
+
+  @protected
+  final DateFormat _dateFormat = DateFormat('H:m:s.S');
+
+  static const appName = 'signleton_pattern_example';
+
+  void log(message, [Object? error, StackTrace? stackTrace]) {
+    _logger.info(message, error, stackTrace);
+  }
+}
+
+/// Singleton wrapper by extension
+///
+class DebugLogger extends BaseLogger {
+  static DebugLogger? _instance;
+
+  /// private constructor
+  ///
+  DebugLogger._internal() {
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen(_recordHandle);
+    _logger = Logger(BaseLogger.appName);
+    debugPrint('<DebugLogger> creation');
+    _instance = this;
+  }
+
+  /// public fatory constructor
+  /// lazy instantation
+  factory DebugLogger() => _instance ??= DebugLogger._internal();
+
+  /// record handler for the logging message content
+  /// it is added in the private constructor
+  void _recordHandle(LogRecord rec) {
+    debugPrint('${_dateFormat.format(rec.time)}: ${rec.message}, ${rec.error}');
+  }
+}
